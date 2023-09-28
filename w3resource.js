5c9994f2096785acce96a95699347a857cc6638c
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-09-28 10:01:15 +0100

    Update w3resource.js

--- w3resource.js
+++ w3resource.js
@@ -1,10 +1,11 @@
+/* Exercise 1 */
 /* Write a javaScript to display the current day and time */
 
 
 
 
 
-
+/* Exercise 2 */
 /* Write a JavaScript program to print the current window contents */
 
 function print_current_page()
@@ -12,5 +13,6 @@ function print_current_page()
   window.print();
 }
 
+/* Exercise 3 */
 /* Write a JavaScript program to get the current date */
 

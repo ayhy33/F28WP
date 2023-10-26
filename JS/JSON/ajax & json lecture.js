a99ed9eafe7269e606ce3aebbe36b4081ce9ffdb
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-10-26 06:56:30 +0100

    Update and rename ajax&json_lecture.js to ajax & json lecture.js

--- JS/JSON/ajax & json lecture.js
+++ JS/JSON/ajax & json lecture.js
@@ -1,3 +1,13 @@
+// Convert JavaScript object into a string
+var json = {
+  person: {
+    name: "bob",
+    age: 20
+  }
+};
+
+console.log(JSON.stringify(json));
+
 // convert JSON string into a JavaScript object
 var json = '{"person" : {"age":20, "name": "bob"}}';
 var parsed = JSON.parse(json);

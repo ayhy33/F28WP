fbbd3be81dc43bd02d089fb335c17493745263bf
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-11-23 04:41:48 +0000

    Update blocking_code.js

--- JS/Node.js/blocking_code.js
+++ JS/Node.js/blocking_code.js
@@ -1,1 +1,13 @@
+/* Create a text file named input.txt */
+Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!
 
+/* Create a js file named main.js */
+var fs = require("fs");
+var data = fs.readFileSync('input.txt');
+
+console.log(data.toString());
+console.log("Program Ended");
+
+/* Run the main.js to see the result */
+$ node main.js

f08053661a3752e887fcbb8e6b7671df0b43dc82
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-11-23 04:46:29 +0000

    Update non-blocking_code.js

--- JS/Node.js/non-blocking_code.js
+++ JS/Node.js/non-blocking_code.js
@@ -1,1 +1,17 @@
+/* Create a text file named input.txt */
+Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!
 
+/* Update main.js */
+var fs = require("fs");
+fs.readFile('input.txt', function (err, data) {
+  if (err) return console.error(err);
+  console.log(data.toString());
+});  
+
+console.log("Program Ended");
+
+/* Verify the output */
+Program Ended
+Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!

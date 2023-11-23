ff4f452231681ffcb870cba831933e70cd905276
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-11-23 05:07:13 +0000

    Update event-loop.js

--- JS/Node.js/event-loop.js
+++ JS/Node.js/event-loop.js
@@ -1,1 +1,35 @@
+/* Event-Driven Programming */
+// Import events module
+var events = require('events');
 
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+
+// Create an event handler as follows
+var connectHandler = function connected() {
+  console.log('connection successful.');
+
+  // Fire the data_received event
+  eventEmitter.emit('data_received');
+}
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function(){
+  console.log('data received successfully.');
+});
+
+// Fire the connenction event
+evenrEmitter.emit('connection');
+
+console.log("Program Ended.");
+
+/* run to check the output */
+$ node main.js
+
+/* Result */
+connection successful.
+data received successfully.
+Program Ended.  

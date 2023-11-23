357880a41623802ab26d2031dc44274e2063bf67
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-11-23 06:26:31 +0000

    Update file_system.js
    reading a file

--- JS/Node.js/file_system.js
+++ JS/Node.js/file_system.js
@@ -121,3 +121,40 @@ Going to write into existing file
 Data written successfully!
 Let's read newly written data
 Asynchronous read: Simply Easy Learning!
+
+/* Reading a File */
+// Create main.js
+var fs = require("fs");
+var buf = new Buffer(1024);
+
+console.log("Going to open an existing file");
+fs.open('input.txt', 'r+', function (err, fd) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("File opened successfully!");
+  console.log("Going to read the file");
+
+  fs.read(fd, buf, 0, buf.length, 0, function (err, bytes) {
+    if (err){
+      console.log(err);
+    }
+    console.log(bytes + " bytes read");
+
+    // Print only read bytes to avoid junk
+          if (bytes > 0) {
+            console.log(buf.slice(0, bytes).toString());
+          }
+  });
+});
+
+// Run main.js
+$ node main.js
+
+// Verify the Output
+Going to open an existing file
+File opened successfully!
+Going to read the file
+97 bytes read
+Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!

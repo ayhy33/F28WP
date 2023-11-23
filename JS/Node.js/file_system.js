1bca5311edc08e6769c36c05038feaae988a85d5
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-11-23 06:08:06 +0000

    Update file_system.js
    Get file information

--- JS/Node.js/file_system.js
+++ JS/Node.js/file_system.js
@@ -50,3 +50,44 @@ $ node main.js
 // Verify the output
 Going to open file!
 File opened successfully!
+
+/* Get File Information */
+// Create main.js 
+var fs = require("fs");
+
+console.log("Going to get file info!");
+fs.stat('input.txt', function (err, stats) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(stats);
+  console.log("Got file info successfully!");
+
+  // Check file type
+  console.log("isFile ? " + stats.isFile());
+  console.log("isDirectory ? " + stats.isDirectory());
+});
+
+/* Run main.js */
+$ node main.js
+
+/* Verify the Output */
+Going to get file info!
+{ 
+   dev: 1792,
+   mode: 33188,
+   nlink: 1,
+   uid: 48,
+   gid: 48,
+   rdev: 0,
+   blksize: 4096,
+   ino: 4318127,
+   size: 97,
+   blocks: 8,
+   atime: Sun Mar 22 2015 13:40:00 GMT-0500 (CDT),
+   mtime: Sun Mar 22 2015 13:40:57 GMT-0500 (CDT),
+   ctime: Sun Mar 22 2015 13:40:57 GMT-0500 (CDT) 
+}
+Got file info successfully!
+isFile ? true
+isDirectory ? false

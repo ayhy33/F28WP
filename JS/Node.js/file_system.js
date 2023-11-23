32836e24810a52668b3bd940ebda91a2b630f3ad
ayhy33 <115602736+ayhy33@users.noreply.github.com>
2023-11-23 06:00:57 +0000

    Update file_system.js
    Synchronous vs Asynchronous

--- JS/Node.js/file_system.js
+++ JS/Node.js/file_system.js
@@ -1,1 +1,33 @@
+/* Synchronous vs Asynchrnous */
+// Create a text file named input.txt
+Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!
+
+// Create a js file named main.js
+var fs = require("fs");
+
+// Asynchronous read
+fs.readFile('input.txt', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("Asynchronous read: " + data.toString());
+});
+
+// Synchronous read
+var data = fs.readFileSync('input.txt');
+console.log("Synchronous read: " + data.toString());
+
+console.log("Program Ended");
+
+// Run the main.js
+$ node main.js
+
+// Verify the output
+Synchronous read: Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!
+
+Program Ended
+Asynchronous read: Tutorials Point is giving self learning content
+to teach the world in simple and easy way!!!!!
 
